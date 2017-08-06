cf3614b5b19120212774b6141caad24cdd602448
dicotha <wbn_k@hotmail.com>
2017-08-06 20:18:15 -0300

    init sistem in angular to my work

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { InsideComponent } from './inside/inside.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    InsideComponent
   ],
   imports: [
     BrowserModule
